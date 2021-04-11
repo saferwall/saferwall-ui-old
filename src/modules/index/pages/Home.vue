888b8847aea81bc4be63da7dbfb455aa065d8e50
rais0005 <yassine.rais@etudiant.univ-reims.fr>
2021-04-11 23:55:41 +0200

    🖋 Working in Upload file section

--- src/modules/index/pages/Home.vue
+++ src/modules/index/pages/Home.vue
@@ -1,16 +1,16 @@
 <template>
-  <!-- <upload-file /> -->
+  <upload-file />
   <latest-activities :activities="getActivities" />
 </template>
 
 <script>
 import LatestActivities from '@/common/components/LatestActivities.vue'
-// import UploadFile from '@/common/components/UploadFile.vue'
+import UploadFile from '@/common/components/UploadFile.vue'
 import { activityGetters, activityMethods } from '@/state/helpers';
 
 export default {
   components:  {
-    // UploadFile,
+    UploadFile,
     LatestActivities
   },
   computed: {
