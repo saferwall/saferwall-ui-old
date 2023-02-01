4f9a8be2beb40e8337859fd3512fadf60476da5b
LordNoteworthy <lord.noteworthy@protonmail.com>
2023-02-02 10:07:41 +1100

    fix: reading avatars from CDN

--- src/common/helpers.js
+++ src/common/helpers.js
@@ -1,68 +1,67 @@
 import Config from "./config";
-import { avgList } from '@/common/data/avg'
+import { avgList } from "@/common/data/avg";
 import { createToast } from "mosha-vue-toastify";
 
 /**
  * Check if name in the list of avg
- * @param {String} name 
+ * @param {String} name
  * @returns {Boolean}
  */
 export function isAnAVG(name) {
-    return avgList.find(avg => avg.toLowerCase() === name.toLowerCase()) !== undefined;
+  return (
+    avgList.find((avg) => avg.toLowerCase() === name.toLowerCase()) !==
+    undefined
+  );
 }
 
 /**
  * Generate avatar url using username
- * 
- * @param {String} username 
- * @returns 
+ *
+ * @param {String} username
+ * @returns
  */
 export const generateAvatar = function (username) {
-    let avatarCdn = Config.value('avatarURL');
-    return `${avatarCdn}${username}`
-}
+  let avatarCdn = Config.value("avatarURL");
+  let userID = username.toLowerCase();
+  return `${avatarCdn}${userID}`;
+};
 
 /**
- * Map tags objects to array 
- * 
- * @param {Object} tags 
- * @returns 
+ * Map tags objects to array
+ *
+ * @param {Object} tags
+ * @returns
  */
 export const mapTags = (tags) => {
-    let tagkeys = Object.keys(tags || {});
+  let tagkeys = Object.keys(tags || {});
 
-    return tagkeys.reduce((final_tags, tagKey) => {
-        let list = tags[tagKey] || [];
+  return tagkeys.reduce((final_tags, tagKey) => {
+    let list = tags[tagKey] || [];
 
-        if (!Array.isArray(list)) {
-            list = [list];
-        }
+    if (!Array.isArray(list)) {
+      list = [list];
+    }
 
-        return list.reduce((_list, tag) => {
-            _list.push({
-                name: tag,
-                avg: isAnAVG(tagKey),
-            });
-            return _list;
-        }, final_tags);
-    }, []);
-}
+    return list.reduce((_list, tag) => {
+      _list.push({
+        name: tag,
+        avg: isAnAVG(tagKey),
+      });
+      return _list;
+    }, final_tags);
+  }, []);
+};
 
 /**
  * Catch auth throw and rethrow
- * 
- * @param {Error} e 
+ *
+ * @param {Error} e
  */
 export const catchAuthThrow = (message) => (e) => {
-    createToast(
-        e.response.status == 401
-            ? message
-            : e.response.message,
-        {
-            type: "warning",
-            position: "bottom-right",
-        }
-    );
+  createToast(e.response.status == 401 ? message : e.response.message, {
+    type: "warning",
+    position: "bottom-right",
+  });
 
-    throw e;
-}
+  throw e;
+};
